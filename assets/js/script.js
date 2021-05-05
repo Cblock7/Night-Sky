893e2d5643d057813c60827adfc671d8d1bf0001
Corey Block <corey.block7@gmail.com>
2021-05-05 13:27:43 -0600

    removed bad code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
-// var mymap = L.map('liveMap').setView([51.505, -0.09], 13);
-
 function moveISS () {
     $.getJSON('http://api.open-notify.org/iss-now.json?callback=?', function(data) {
         var lat = data['iss_position']['latitude'];
         var lon = data['iss_position']['longitude'];
 
-        // See leaflet docs for setting up icons and map layers
-        // The update to the map is done here:
+    
         iss.setLatLng([lat, lon]);
         isscirc.setLatLng([lat, lon]);
         map.panTo([lat, lon], animate=true);
