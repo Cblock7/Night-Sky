1659b743391306fc72d9e1b58d2dd405d72939f9
Corey Block <corey.block7@gmail.com>
2021-05-05 13:22:08 -0600

    javascript to add the live-map with current ISS location

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// var mymap = L.map('liveMap').setView([51.505, -0.09], 13);
+
+function moveISS () {
+    $.getJSON('http://api.open-notify.org/iss-now.json?callback=?', function(data) {
+        var lat = data['iss_position']['latitude'];
+        var lon = data['iss_position']['longitude'];
+
+        // See leaflet docs for setting up icons and map layers
+        // The update to the map is done here:
+        iss.setLatLng([lat, lon]);
+        isscirc.setLatLng([lat, lon]);
+        map.panTo([lat, lon], animate=true);
+    });
+    setTimeout(moveISS, 5000); 
+}
+
+var map = L.map('liveMap').setView([0,0], 2);
+var iss = L.marker([0, 0], ).addTo(map);
+var isscirc = L.circle([0,0], 2200e3, {color: "#c22", opacity: 0.3, weight:1, fillColor: "#c22", fillOpacity: 0.1}).addTo(map); 
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+
+moveISS();
